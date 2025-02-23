2975781a6f2f62ee9f7ae12fb8246e0b019957f6
dayohq <smartjoe59@gmail.com>
2025-02-23 18:13:28 +0100

    Enhanced relationship_app by creating FBV & CBV.

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list books, name='list_books'), # Function-based view (FBV)
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+
+def list_books(request):
+    """Function-based view to list all books."""
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(Detailview):
+    """Class-based view to display details of a specific library."""
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
