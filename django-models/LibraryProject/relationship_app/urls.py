9f75363b76bb4baec153c6ae75e5a4fdf0343031
dayohq <smartjoe59@gmail.com>
2025-02-24 22:01:02 +0100

    recent change to relationship_app.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,5 +3,5 @@ from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', list books, name='list_books'), # Function-based view (FBV)
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), # Class-based View CBV
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library, Book
 
 # Create your views here.
-from .models import Book
+
 
 def list_books(request):
     """Function-based view to list all books."""
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-from django.views.generic import DetailView
-from .models import Library
+    return render(request, 'list_books.html', {'books': books})
 
 class LibraryDetailView(Detailview):
     """Class-based view to display details of a specific library."""
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'library_detail.html'
     context_object_name = 'library'
