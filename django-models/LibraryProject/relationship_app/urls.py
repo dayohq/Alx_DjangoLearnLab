4f68507f9ac5100b2eaafe2e66f2dd967194ecb2
dayohq <smartjoe59@gmail.com>
2025-02-25 21:54:02 +0100

    Added user authentication (login, logout, registration)

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/', list books, name='list_books'), # Function-based view (FBV)
+    path('books/', list_books, name='list_books'), # Function-based view (FBV)
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), # Class-based View CBV
+]
+
+from .views import user_login, user_logout, register
+
+urlpatterns = [
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,3 +16,40 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+# User Login View
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, "authentication/login.html", {"form": form})
+
+
+# User Logout View
+def user_logout(request):
+    logout(request)
+    return redirect('/login/')
+
+
+# User Registration View
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, "authentication/register.html", {"form": form})
+    
