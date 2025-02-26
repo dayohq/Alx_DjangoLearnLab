0ec128574692ca7532c52c0d1a3d121c9eeccc74
dayohq <smartjoe59@gmail.com>
2025-02-26 22:09:50 +0100

    Modificatin 2 to user authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
+    # Book-related views
     path('books/', list_books, name='list_books'), # Function-based view (FBV)
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), # Class-based View CBV
-]
 
-from .views import user_login, user_logout, register
-
-urlpatterns = [
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
+    # Authentication views
     path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='authentication/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='authentication/logout.html'), name='logout'),
 ]
