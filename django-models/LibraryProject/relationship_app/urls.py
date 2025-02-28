1c0707d503910793c73d8da1259b0f4d30933f37
dayohq <smartjoe59@gmail.com>
2025-02-28 20:44:14 +0100

    6th modification to authentication app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView, user_login, user_logout, register
 
 urlpatterns = [
