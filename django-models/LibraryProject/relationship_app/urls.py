d61f44a5dd5c5f872eb17813488db6c332fdc03b
dayohq <smartjoe59@gmail.com>
2025-03-01 09:05:36 +0100

    12th

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register
+from .views import register, list_books, LibraryDetailView
 
 
 urlpatterns = [
