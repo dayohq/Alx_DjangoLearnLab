578bd3acc542102ebe16aef6071bb11f18f4f38a
dayohq <smartjoe59@gmail.com>
2025-03-08 16:52:43 +0100

    Authentication and permissions in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -16,3 +17,4 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] # Only authenticated users can access
