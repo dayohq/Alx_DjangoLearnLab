ab4d72e53b18bea516414321fcefb265e35df964
dayohq <smartjoe59@gmail.com>
2025-03-08 16:31:12 +0100

    CRUD operations with ViewSets and Routers in Django DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
+    """
+    A ViewSet for viewing and editing Book instances.
+    Handles all CRUD operations: list, create, retrieve, update, and delete.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
