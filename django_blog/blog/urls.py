b4f06d96d9bc22b16a5818a0e14cbe707a3ceca7
dayohq <smartjoe59@gmail.com>
2025-04-01 11:57:18 +0100

    modification to comment feature

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
 
     # Comments view urls
     path("post/<int:pk>/comment/new/", CommentCreateView.as_view(), name="comment-create"),
-    path("comment/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
