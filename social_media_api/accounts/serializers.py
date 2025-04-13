b37874dda7e2f1eda3b1d3559db0a2f1ec229ccb
dayohq <smartjoe59@gmail.com>
2025-04-13 19:55:23 +0100

    Modification to social media api project environment.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 
-User = get_user_model()
+User = get_user_model().objects.create_user
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
